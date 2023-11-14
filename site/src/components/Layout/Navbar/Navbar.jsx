be2d6511854eb917bde7f409914f2f2fbdc563ee
Sadiqovilkin <sadiqovilkin068@gmail.com>
2023-11-14 20:43:22 +0400

    #Aded layout

---
+++ site/src/components/Layout/Navbar/Navbar.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <nav>
+        <div className="container">
+            <div className="row">
+                <div className="col-3">
+                    <div className="fullnamae">
+                        <h2>Ilkin Sadigzade</h2>
+                    </div>
+                </div>
+                <div className="col-9"></div>
+            </div>
+        </div>
+    </nav>
+  )
+}
+
+export default Navbar
--- site/src/index.js
+++ site/src/index.js
@@ -6,10 +6,13 @@ import App from './App';
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import "../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js";
 import "../node_modules/bootstrap/dist/js/bootstrap.min.js";
+import Layout from './components/Layout/Layout.js';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
+   <Layout>
     <App />
+    </Layout>
   </BrowserRouter>
 );
 

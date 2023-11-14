e44f08f00a281f2864bbe075c6f66d56e7003b71
Sadiqovilkin <sadiqovilkin068@gmail.com>
2023-11-14 23:41:53 +0400

    #finished Banner section 1/2

--- site/src/components/Pages/Home/Banner/Banner.jsx
+++ site/src/components/Pages/Home/Banner/Banner.jsx
@@ -1,10 +1,40 @@
-import React from 'react'
-
+import React from 'react';
+import { FaLinkedin } from "react-icons/fa";
+import { FaGithub } from "react-icons/fa";
+import { FaInstagram } from "react-icons/fa";
+import png from '../../../../image/PortrePng.jpeg'
 const Banner = () => {
   return (
-    <div>Banner
-      
+   <section id='banner'>
+    <div className="container ">
+      <div className="blur"></div>
+      <div className="row justify-content-center">
+        <div className="col-4">
+          <div className="banner_card">
+            <div className="card_img">
+              <img src={png} alt="" />
+            </div>
+            <div className="card_desc">
+              <h1>Ilkin Sadigzade</h1>
+              <p>Front-end Developer </p>
+              <div className="social_media">
+                <a href="#"> <FaInstagram/></a>
+                <a href="#"> <FaGithub/></a>
+                <a href="#"> <FaLinkedin/></a>
+               
+               
+               
+              </div>
+            </div>
+            <div className="card_btns">
+              <a href="" download>Dowland Cv</a>
+              <a href="#contact">Hire me</a>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
+   </section>
   )
 }
 

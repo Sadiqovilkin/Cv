90208a494c8324f726252e40be8dd11865b8fc41
Sadiqovilkin <sadiqovilkin068@gmail.com>
2023-11-14 20:58:31 +0400

    #Finished Standart Navbar

--- site/src/components/Layout/Navbar/Navbar.jsx
+++ site/src/components/Layout/Navbar/Navbar.jsx
@@ -10,7 +10,18 @@ const Navbar = () => {
                         <h2>Ilkin Sadigzade</h2>
                     </div>
                 </div>
-                <div className="col-9"></div>
+                <div className="col-9">
+                    <div className="navbar_items">
+                        <ul>
+                            <li><a href="#about">About</a></li>
+                            <li><a href="#skills">skills</a></li>
+                            <li><a href="#experience">Experience</a></li>
+                            <li><a href="#education">education</a></li>
+                            <li><a href="#portfolio">Portfolio</a></li>
+                            <li><a href="#contact">contact</a></li>
+                        </ul>
+                    </div>
+                </div>
             </div>
         </div>
     </nav>
--- site/src/index.js
+++ site/src/index.js
@@ -7,6 +7,7 @@ import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import "../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js";
 import "../node_modules/bootstrap/dist/js/bootstrap.min.js";
 import Layout from './components/Layout/Layout.js';
+import "./components/scss/style.scss";
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
